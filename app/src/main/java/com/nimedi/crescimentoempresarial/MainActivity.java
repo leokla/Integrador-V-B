e9075315636afbd8c17ed243aa6a032a1a8ed798
Rafael Dal Pra <rafael@sparkag.com.br>
2021-11-25 08:58:25 -0300

    Api para botões google e facebook

--- app/src/main/java/com/nimedi/crescimentoempresarial/MainActivity.java
+++ app/src/main/java/com/nimedi/crescimentoempresarial/MainActivity.java
@@ -2,7 +2,10 @@ package com.nimedi.crescimentoempresarial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void googleOnClick(View view){
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com.br/")));
+    }
+
+    public void faceOnClick(View view){
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://pt-br.facebook.com/")));
     }
 }
